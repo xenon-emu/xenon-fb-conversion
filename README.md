5bda5cc032e6a632d202b62321c695c80d216800
¥IGA <164882787+Xphalnos@users.noreply.github.com>
2025-03-31 23:07:32 +0200

    [...] Rework main and update deps

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # xenon-fb-conversion
+
 ## What is this?
-### It's a tool used to visualize the Xenos Framebuffer from the Xbox 360
-### Currently, it's a part of the test suite 
+
+It's a tool used to visualize the Xenos Framebuffer from the Xbox 360.
+
+## How to use it?
+
+Just drag and drop `fbmem.bin` onto the executable.
--- main.cpp
+++ main.cpp
@@ -1,40 +1,43 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <stdint.h>
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
+// Copyright 2025 Xenon Emulator Project
+
 #include <SDL3/SDL.h>
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+
 #define GL_GLEXT_PROTOTYPES
+extern "C" {
 #include <KHR/khrplatform.h>
 #include <glad/glad.h>
+}
+
 #define TILE(x) ((((x) + 31) >> 5) << 5)
+
+constexpr int resWidth = TILE(1280);
+constexpr int resHeight = TILE(720);
+constexpr int internalWidth = 1280;
+constexpr int internalHeight = 720;
 
 SDL_Window* window;
 SDL_GLContext context;
 GLuint texture, shaderProgram, pixelBuffer;
 GLuint dummyVAO, renderShaderProgram;
-int resWidth = TILE(1280);
-int resHeight = TILE(720);
-int internalWidth = 1280;
-int internalHeight = 720;
 
-int initSdl(const char* windowName, int w, int h, SDL_WindowFlags flags)
-{
-    if (!SDL_Init(SDL_INIT_VIDEO))
-    {
-        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
-        return 1;
-    }
+int initSDL(const char* windowName, const int w, const int h, SDL_WindowFlags flags) {
+  if (!SDL_Init(SDL_INIT_VIDEO)) {
+    SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+    return 1;
+  }
 
-    window = SDL_CreateWindow(windowName, w, h, flags);
+  window = SDL_CreateWindow(windowName, w, h, flags);
 
-    if (!window)
-    {
-        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
-        return 1;
-    }
-    return 0;
+  if (!window) {
+    SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+    return 1;
+  }
+
+  return 0;
 }
 
 constexpr const char* vertexShaderSource = R"(
@@ -47,8 +50,8 @@ out vec2 o_texture_coord;
 void main() {
   o_texture_coord = vec2((gl_VertexID << 1) & 2, gl_VertexID & 2);
   gl_Position = vec4(o_texture_coord * vec2(2.0f, -2.0f) + vec2(-1.0f, 1.0f), 0.0f, 1.0f);
-}
-)";
+})";
+
 constexpr const char* fragmentShaderSource = R"(
 #version 430 core
 
@@ -65,8 +68,8 @@ void main() {
   float g = float((pixel >> 8) & 0xFF) / 255.0;
   float b = float((pixel >> 0) & 0xFF) / 255.0;
   o_color = vec4(r, g, b, a);
-}
-)";
+})";
+
 constexpr const char* computeShaderSource = R"(
 #version 430 core
 
@@ -102,12 +105,12 @@ void main() {
     return;
 
   // Precalc whatever it would be with extra sizing for 32x32 tiles
-  int tiledWidth = TILE(internalWidth);
-  int tiledHeight = TILE(internalHeight);
+  const int tiledWidth = TILE(internalWidth);
+  const int tiledHeight = TILE(internalHeight);
 
   // Scale accordingly
-  float scaleX = tiledWidth / float(resWidth);
-  float scaleY = tiledHeight / float(resHeight);
+  const float scaleX = tiledWidth / float(resWidth);
+  const float scaleY = tiledHeight / float(resHeight);
 
   // Map to source resolution
   int srcX = int(float(texel_pos.x) * scaleX);
@@ -119,198 +122,184 @@ void main() {
 
   uint packedColor = pixel_data[xeIndex];
   imageStore(o_texture, texel_pos, uvec4(packedColor, 0, 0, 0));
-}
-)";
+})";
 
-void compileShader(GLuint shader, const char* source)
-{
-    glShaderSource(shader, 1, &source, nullptr);
-    glCompileShader(shader);
+void compileShader(GLuint shader, const char* source) {
+  glShaderSource(shader, 1, &source, nullptr);
+  glCompileShader(shader);
 
-    int success;
-    char infoLog[512];
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(shader, 512, NULL, infoLog);
-        std::cout << "Shader Compilation Error:\n" << infoLog << std::endl;
-    }
+  int success;
+  char infoLog[512];
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(shader, 512, NULL, infoLog);
+    std::cout << "Shader Compilation Error:\n" << infoLog << std::endl;
+  }
 }
 
-GLuint createShaderProgram(const char* vertex, const char* fragment)
-{
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    compileShader(vertexShader, vertex);
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    compileShader(fragmentShader, fragment);
-    GLuint program = glCreateProgram();
-    glAttachShader(program, vertexShader);
-    glAttachShader(program, fragmentShader);
-    glLinkProgram(program);
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-    return program;
+GLuint createShaderProgram(const char* vertex, const char* fragment) {
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  compileShader(vertexShader, vertex);
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  compileShader(fragmentShader, fragment);
+  GLuint program = glCreateProgram();
+  glAttachShader(program, vertexShader);
+  glAttachShader(program, fragmentShader);
+  glLinkProgram(program);
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+  return program;
 }
 
-void initShaders()
-{
-    GLuint computeShader = glCreateShader(GL_COMPUTE_SHADER);
-    compileShader(computeShader, computeShaderSource);
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, computeShader);
-    glLinkProgram(shaderProgram);
-    glDeleteShader(computeShader);
-    renderShaderProgram = createShaderProgram(vertexShaderSource, fragmentShaderSource);
+void initShaders() {
+  GLuint computeShader = glCreateShader(GL_COMPUTE_SHADER);
+  compileShader(computeShader, computeShaderSource);
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, computeShader);
+  glLinkProgram(shaderProgram);
+  glDeleteShader(computeShader);
+  renderShaderProgram = createShaderProgram(vertexShaderSource, fragmentShaderSource);
 }
 
-void initTexture()
-{
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexStorage2D(GL_TEXTURE_2D, 1, GL_R32UI, resWidth, resHeight);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glBindImageTexture(0, texture, 0, GL_FALSE, 0, GL_READ_WRITE, GL_R32UI);
-    glBindBuffer(GL_TEXTURE_2D, 0);
+void initTexture() {
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D, texture);
+  glTexStorage2D(GL_TEXTURE_2D, 1, GL_R32UI, resWidth, resHeight);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glBindImageTexture(0, texture, 0, GL_FALSE, 0, GL_READ_WRITE, GL_R32UI);
+  glBindBuffer(GL_TEXTURE_2D, 0);
 }
 
 // ARGB (Console is BGRA)
-#define COLOR(r, g, b, a) ((a) << 24 | (r) << 16 | (g) << 8  | (b) << 0)
-int pitch = resWidth * resHeight * sizeof(uint32_t);
-std::vector<uint32_t> pixels(pitch, COLOR(30, 30, 30, 255)); // Init with dark grey
-void initPixelBuffer()
-{
-    glGenBuffers(1, &pixelBuffer);
-    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, pixelBuffer);
-    glBufferData(GL_SHADER_STORAGE_BUFFER, pixels.size(), pixels.data(), GL_DYNAMIC_DRAW);
-    glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+#define COLOR(r, g, b, a) ((a) << 24 | (r) << 16 | (g) << 8 | (b) << 0)
+
+constexpr int pitch = resWidth * resHeight * sizeof(const uint32_t);
+const std::vector<uint32_t> pixels(pitch, COLOR(30, 30, 30, 255)); // Init with dark grey
+
+void initPixelBuffer() {
+  glGenBuffers(1, &pixelBuffer);
+  glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, pixelBuffer);
+  glBufferData(GL_SHADER_STORAGE_BUFFER, pixels.size(), pixels.data(), GL_DYNAMIC_DRAW);
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
 }
 
-void computeDispatch()
-{
-    glUseProgram(shaderProgram);
-    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, pixelBuffer);
-    glUniform1i(glGetUniformLocation(shaderProgram, "internalWidth"), internalWidth);
-    glUniform1i(glGetUniformLocation(shaderProgram, "internalHeight"), internalHeight);
-    glUniform1i(glGetUniformLocation(shaderProgram, "resWidth"), resWidth);
-    glUniform1i(glGetUniformLocation(shaderProgram, "resHeight"), resHeight);
-    glDispatchCompute(resWidth / 16, resHeight / 16, 1);
-    glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT | GL_TEXTURE_FETCH_BARRIER_BIT);
+void computeDispatch() {
+  glUseProgram(shaderProgram);
+  glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, pixelBuffer);
+  glUniform1i(glGetUniformLocation(shaderProgram, "internalWidth"), internalWidth);
+  glUniform1i(glGetUniformLocation(shaderProgram, "internalHeight"), internalHeight);
+  glUniform1i(glGetUniformLocation(shaderProgram, "resWidth"), resWidth);
+  glUniform1i(glGetUniformLocation(shaderProgram, "resHeight"), resHeight);
+  glDispatchCompute(resWidth / 16, resHeight / 16, 1);
+  glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT | GL_TEXTURE_FETCH_BARRIER_BIT);
 }
 
-void initOpenGL()
-{
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
-    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+void initOpenGL() {
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+  SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+  SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
-    context = SDL_GL_CreateContext(window);
-    if (!context)
-    {
-        SDL_Log("Failed to create OpenGL context: %s", SDL_GetError());
-        exit(-1);
+  context = SDL_GL_CreateContext(window);
+
+  if (!context) {
+    SDL_Log("Failed to create OpenGL context: %s", SDL_GetError());
+    exit(-1);
+  }
+
+  if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+    SDL_Log("Failed to initialize GLAD");
+    exit(-1);
+  }
+
+  initShaders();
+  initTexture();
+  initPixelBuffer();
+  // Creat a dummy VAO
+  glGenVertexArrays(1, &dummyVAO);
+  // Setup viewport
+  glViewport(0, 0, resWidth, resHeight);
+  glDisable(GL_BLEND);
+  glDisable(GL_DEPTH_TEST);
+
+  glClearColor(0.7f, 0.7f, 0.7f, 1.f);
+
+  SDL_GL_SetSwapInterval(0);
+  SDL_SetWindowFullscreen(window, false);
+}
+
+void passPixelBuffer(uint32_t* data, size_t size) {
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, pixelBuffer);
+  glBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, pitch, data);
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+}
+
+const std::unique_ptr<uint8_t[]> buffer = std::make_unique<uint8_t[]>(pitch * 4);
+
+void render() {
+  // Send over the buffer with the 360fb that is swizzled
+  passPixelBuffer(reinterpret_cast<uint32_t*>(buffer.get()), pitch);
+
+  // Dispatch compute shader to unswizzle data
+  computeDispatch();
+
+  // Stop anything from updating texture after finishing CS
+  glMemoryBarrier(GL_TEXTURE_UPDATE_BARRIER_BIT);
+
+  // Draw fullscreen rect
+  glUseProgram(renderShaderProgram);
+  glBindTexture(GL_TEXTURE_2D, texture);
+  glBindVertexArray(dummyVAO);
+  glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+
+  // Swap
+  SDL_GL_SwapWindow(window);
+}
+
+void shutdownRender() {
+  glDeleteProgram(shaderProgram);
+  SDL_GL_DestroyContext(context);
+  SDL_DestroyWindow(window);
+}
+
+int main() {
+  std::cout << "Width: " << resWidth << std::endl;
+  std::cout << "Height: " << resHeight << std::endl;
+  SDL_WindowFlags flags = SDL_WINDOW_OPENGL;
+  if (initSDL("Xenon FB Conversion", resWidth, resHeight, flags) != 0) {
+    return 1;
+  }
+
+  std::ifstream f("fbmem.bin", std::ios::in | std::ios::binary);
+
+  if (!f) {
+    std::cout << "Failed to open framebuffer dump!" << std::endl;
+  } else {
+    f.read(reinterpret_cast<char*>(buffer.get()), pitch * 4);
+  }
+  f.close();
+
+  initOpenGL();
+
+  bool running = true;
+  SDL_Event event;
+  while (running) {
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_EVENT_QUIT)
+        running = false;
     }
+    render();
+  }
 
-    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress))
-    {
-        SDL_Log("Failed to initialize GLAD");
-        exit(-1);
-    }
-    initShaders();
-    initTexture();
-    initPixelBuffer();
-    // Creat a dummy VAO
-    glGenVertexArrays(1, &dummyVAO);
-    // Setup viewport
-    glViewport(0, 0, resWidth, resHeight);
-    glDisable(GL_BLEND);
-    glDisable(GL_DEPTH_TEST);
-
-    glClearColor(0.7f, 0.7f, 0.7f, 1.f);
-
-    SDL_GL_SetSwapInterval(0);
-    SDL_SetWindowFullscreen(window, false);
-}
-
-void passPixelBuffer(uint32_t* data, size_t size)
-{
-    glBindBuffer(GL_SHADER_STORAGE_BUFFER, pixelBuffer);
-    glBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, pitch, data);
-    glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
-}
-
-std::unique_ptr<uint8_t[]> buffer = std::make_unique<uint8_t[]>(pitch * 4);
-void render()
-{
-    // Send over the buffer with the 360fb that is swizzled
-    passPixelBuffer(reinterpret_cast<uint32_t*>(buffer.get()), pitch);
-
-    // Dispatch compute shader to unswizzle data
-    computeDispatch();
-
-    // Stop anything from updating texture after finishing CS
-    glMemoryBarrier(GL_TEXTURE_UPDATE_BARRIER_BIT);
-
-    // Draw fullscreen rect
-    glUseProgram(renderShaderProgram);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glBindVertexArray(dummyVAO);
-    glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-
-    // Swap
-    SDL_GL_SwapWindow(window);
-}
-
-void shutdownRender()
-{
-    glDeleteProgram(shaderProgram);
-    SDL_GL_DestroyContext(context);
-    SDL_DestroyWindow(window);
-}
-
-int main()
-{
-    std::cout << "Width: " << resWidth << std::endl;
-    std::cout << "Height: " << resHeight << std::endl;
-    SDL_WindowFlags flags = SDL_WINDOW_OPENGL;
-    if (initSdl("OpenGL Window", resWidth, resHeight, flags) != 0)
-        return 1;
-
-    std::ifstream f("fbmem.bin", std::ios::in | std::ios::binary);
-    if (!f)
-    {
-        std::cout << "Failed to open framebuffer dump" << std::endl;
-    }
-    else
-    {
-        f.read(reinterpret_cast<char*>(buffer.get()), pitch * 4);
-    }
-    f.close();
-
-    initOpenGL();
-
-    bool running = true;
-    SDL_Event event;
-    while (running)
-    {
-        while (SDL_PollEvent(&event))
-        {
-            if (event.type == SDL_EVENT_QUIT)
-                running = false;
-        }
-
-        render();
-    }
-
-    shutdownRender();
-    SDL_Quit();
-    return 0;
+  shutdownRender();
+  SDL_Quit();
+  return 0;
 }
